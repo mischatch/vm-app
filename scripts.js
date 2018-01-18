8bb4853499339aa71e5039fe9cd054e1c63eaa05
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-01-18 16:41:13 -0500

    finished dragging multiple items and highlighting drop areas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$( document ).ready(function(){
+$(document).ready(function(){
     // console.log('ready!');
 
 
@@ -36,91 +36,63 @@ $( document ).ready(function(){
         `);
     });
     $('.table').last().children().first().next().remove(); // removing first row
+
+
+    var selectedClass = 'ui-state-highlight',
+         clickDelay = 600,     // click time (milliseconds)
+         lastClick, diffClick; // timestamps
+
+     $(".table-row").on({
+           mousedown: function(e){
+               lastClick = e.timeStamp;
+             },
+           mouseup: function(e){
+             diffClick = e.timeStamp - lastClick;
+             if ( diffClick < clickDelay ) {
+                 $(this).toggleClass(selectedClass);
+               }
+             }
+         })
+         .draggable({
+             revertDuration: 10, // grouped items animate separately, so leave this number low
+             containment: '.tables',
+             start: function(e, ui) {
+                 ui.helper.addClass(selectedClass);
+             },
+             stop: function(e, ui) {
+                 $('.' + selectedClass).css({ top:0, left:0 });
+             },
+             drag: function(e, ui) {
+                 $('.' + selectedClass).css({
+                     top : ui.position.top,
+                     left: ui.position.left
+                 });
+             }
+         });
+
+     $("#sortable1, #sortable2")
+          .droppable({
+             drop: function(e, ui) {
+                 $('.' + selectedClass)
+                  .appendTo($(this))
+                  .add(ui.draggable)
+                  .removeClass(selectedClass)
+                  .css({ top:0, left:0 });
+             },
+             activate: () => { // highlight tables while dragging
+                 $( "#sortable1, #sortable2, #make-drag" ).css({
+                   'background-color': 'rgba(255, 202, 0, 0.2)',
+                 });
+               },
+             deactivate: () => { // dehighlight
+               $( "#sortable1, #sortable2, #make-drag" ).css({
+                 'background-color': ''
+               });
+             },
+           }).sortable();
   });
-
-
-  // $("#sortable1, #sortable2").sortable({
-  //   items: '> :not(.tableheader)',
-  //   connectWith: '.table',
-  //   activate: () => { // highlight tables while dragging
-  //     $( "#sortable1, #sortable2, #make-drag" ).css({
-  //       'background-color': 'rgba(255, 202, 0, 0.2)',
-  //     });
-  //   },
-  //   deactivate: () => { // dehighlight
-  //     $( "#sortable1, #sortable2, #make-drag" ).css({
-  //       'background-color': ''
-  //     });
-  //   },
-  // }).disableSelection();
-
-  $('.table').find('> .table-row').on({
-    click: (e) => {
-      console.log('yo');
-    }
-  });
-
-
-
-    // var selectedClass = 'ui-state-highlight',
-    //      clickDelay = 600,     // click time (milliseconds)
-    //      lastClick, diffClick; // timestamps
-    //
-    //  $("#sortable1").on({
-    //        mousedown: function(e){
-    //          debugger
-    //          // console.log('click');
-    //            lastClick = e.timeStamp;
-    //          },
-    //        mouseup: function(e){
-    //          diffClick = e.timeStamp - lastClick;
-    //          if ( diffClick < clickDelay ) {
-    //              // add selected class to group draggable objects
-    //              $(this).toggleClass(selectedClass);
-    //            }
-    //          }
-    //      })
-    //      .draggable({
-    //          revertDuration: 10, // grouped items animate separately, so leave this number low
-    //          containment: '.tables',
-    //          start: function(e, ui) {
-    //            debugger
-    //              ui.helper.addClass(selectedClass);
-    //          },
-    //          stop: function(e, ui) {
-    //              // reset group positions
-    //              debugger
-    //              $('.' + selectedClass).css({ top:0, left:0 });
-    //          },
-    //          drag: function(e, ui) {
-    //            debugger
-    //              // set selected group position to main dragged object
-    //              // this works because the position is relative to the starting position
-    //              $('.' + selectedClass).css({
-    //                  top : ui.position.top,
-    //                  left: ui.position.left
-    //              });
-    //          }
-    //      });
-    //
-    //  $("#sortable1, #sortable2")
-    //      .sortable()
-    //      .droppable({
-    //          drop: function(e, ui) {
-    //            debugger
-    //              $('.' + selectedClass)
-    //               .appendTo($(this))
-    //               .add(ui.draggable) // ui.draggable is appended by the script, so add it after
-    //               .removeClass(selectedClass)
-    //               .css({ top:0, left:0 });
-    //          }
-    //      });
-    //
-    //
-    //
-    //
-
 });
+
 
 function search(search_table){
   let input = $(`.${search_table}`).val().toLowerCase();
