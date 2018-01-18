7f1b094ffdf436400a1e339b6cac1950de56356b
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-01-17 23:58:04 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-$(function() {
+$( document ).ready(function(){
     // console.log('ready!');
+
 
     var root = 'https://jsonplaceholder.typicode.com';
 
@@ -10,13 +11,13 @@ $(function() {
     data.forEach(album => { // promise distribute data from ajax request
 
       $('.table').first().append(`
-        <div id='make-drag' class='table__row'>
+        <div id='make-drag' class='table-row'>
           <div class='table__cell table__cell--short'>${album.id}</div>
           <div class='table__cell'>${album.title}</div>
         </div>
         `);
     });
-    $('.table .table__row').first().remove(); // removing first row
+    $('.table .table-row').first().remove(); // removing first row
   });
 
 
@@ -28,7 +29,7 @@ $(function() {
     data.forEach(album => { // promise distribute data from ajax request
 
       $('.table').first().next().append(`
-        <div id='make-drag' class='table__row'>
+        <div id='make-drag' class='table-row'>
           <div class='table__cell table__cell--short'>${album.id}</div>
           <div class='table__cell'>${album.title}</div>
         </div>
@@ -38,31 +39,86 @@ $(function() {
   });
 
 
-  $( "#sortable1, #sortable2" ).sortable({
-    items: '#make-drag',
-    connectWith: '.table',
-    activate: () => { // highlight tables while dragging
-      $( "#sortable1, #sortable2, #make-drag" ).css({
-        'background-color': 'rgba(255, 202, 0, 0.2)',
-      });
-    },
-    deactivate: () => { // dehighlight
-      $( "#sortable1, #sortable2, #make-drag" ).css({
-        'background-color': ''
-      });
+  // $("#sortable1, #sortable2").sortable({
+  //   items: '> :not(.tableheader)',
+  //   connectWith: '.table',
+  //   activate: () => { // highlight tables while dragging
+  //     $( "#sortable1, #sortable2, #make-drag" ).css({
+  //       'background-color': 'rgba(255, 202, 0, 0.2)',
+  //     });
+  //   },
+  //   deactivate: () => { // dehighlight
+  //     $( "#sortable1, #sortable2, #make-drag" ).css({
+  //       'background-color': ''
+  //     });
+  //   },
+  // }).disableSelection();
+
+  $('.table').find('> .table-row').on({
+    click: (e) => {
+      console.log('yo');
     }
-  }).disableSelection();
-
-
-  $("#make-drag").draggable({
-    revert: true,
-    fixed: true,
   });
 
-  // $('.table__header').draggable({'revert': 'invalid' });
 
 
-
+    // var selectedClass = 'ui-state-highlight',
+    //      clickDelay = 600,     // click time (milliseconds)
+    //      lastClick, diffClick; // timestamps
+    //
+    //  $("#sortable1").on({
+    //        mousedown: function(e){
+    //          debugger
+    //          // console.log('click');
+    //            lastClick = e.timeStamp;
+    //          },
+    //        mouseup: function(e){
+    //          diffClick = e.timeStamp - lastClick;
+    //          if ( diffClick < clickDelay ) {
+    //              // add selected class to group draggable objects
+    //              $(this).toggleClass(selectedClass);
+    //            }
+    //          }
+    //      })
+    //      .draggable({
+    //          revertDuration: 10, // grouped items animate separately, so leave this number low
+    //          containment: '.tables',
+    //          start: function(e, ui) {
+    //            debugger
+    //              ui.helper.addClass(selectedClass);
+    //          },
+    //          stop: function(e, ui) {
+    //              // reset group positions
+    //              debugger
+    //              $('.' + selectedClass).css({ top:0, left:0 });
+    //          },
+    //          drag: function(e, ui) {
+    //            debugger
+    //              // set selected group position to main dragged object
+    //              // this works because the position is relative to the starting position
+    //              $('.' + selectedClass).css({
+    //                  top : ui.position.top,
+    //                  left: ui.position.left
+    //              });
+    //          }
+    //      });
+    //
+    //  $("#sortable1, #sortable2")
+    //      .sortable()
+    //      .droppable({
+    //          drop: function(e, ui) {
+    //            debugger
+    //              $('.' + selectedClass)
+    //               .appendTo($(this))
+    //               .add(ui.draggable) // ui.draggable is appended by the script, so add it after
+    //               .removeClass(selectedClass)
+    //               .css({ top:0, left:0 });
+    //          }
+    //      });
+    //
+    //
+    //
+    //
 
 });
 
